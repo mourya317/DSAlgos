91d2df1fe68ff2f2546bcbbf3fa35511a177c749
mballa <mourya317@gmail.com>
2017-11-05 00:04:29 +0530

    String problems

--- src/StringProblems/StringWildcardMatching.java
+++ src/StringProblems/StringWildcardMatching.java
@@ -5,15 +5,41 @@ package StringProblems;
  */
 public class StringWildcardMatching {
     public static void main(String[] args){
-        System.out.print(isMatched("mourya","m*r?a"));
+        System.out.print(isStringMatched("mourya","m**r?a*"));
     }
 
-    private static boolean isMatched(String s,String p){
+        private static boolean isStringMatched(String text, String pat) {
+            int l= text.length();
+            int n= pat.length();
+            int starIndex= -1;
+            int iIndex= -1;
+            int i= 0;
+            int j= 0;
+            while(i<l) {
+                if(j<n && ((text.charAt(i)== pat.charAt(j)) || (pat.charAt(j)== '?'))) {
+                    j++;
+                    i++;
+                } else if(j<n && pat.charAt(j)== '*') {
+                    starIndex= j;
+                    iIndex= i;
+                    j++;
+                } else if(starIndex!= -1) {
+                    j= starIndex+1;
+                    i= iIndex+1;
+                    iIndex++;
+                } else {
+                    return false;
+                }
+            }
 
+            while(j<n && pat.charAt(j)== '*') {
+                j++;
+            }
 
-
-
-
-        return false;
+            if(j!= n) {
+                return false;
+            }
+            return true;
+        }
     }
-}
+
